d37efb42131fa8a2d3c4b1f716e709cb7049cbfc
黃功諺 <gy.huang0620@gmail.com>
2026-04-06 19:35:49 +0800

    完成

---
+++ s1142025_黃功諺.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+class Point {
+public:
+    int x;
+    int y;
+
+
+    Point(int xValue, int yValue) {
+        x = xValue;
+        y = yValue;
+    }
+
+
+    Point operator*(Point otherPoint) {
+        return Point(x * otherPoint.x, y * otherPoint.y);
+    }
+
+    void print() {
+        cout << "(" << x << "," << y << ")" << endl;
+    }
+};
+
+int main() {
+    Point p1(1, 2);
+    Point p2(3, 4);
+
+
+    Point result = p1 * p2;
+
+  
+    result.print();
+
+    return 0;
+}
